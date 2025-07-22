906749b199198306ea929240219950996704eaff
antoniomiletic321 <antoniomiletic321@gmail.com>
2025-07-22 13:16:19 +0200

    New generated project from Spring Initializr

---
+++ src/main/java/com/roadmaphs/caching_proxy/CachingProxyApplication.java
@@ -0,0 +1,13 @@
+package com.roadmaphs.caching_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingProxyApplication.class, args);
+	}
+
+}
